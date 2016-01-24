ab6aeaf2b8424ee5cc182b4480fd4e77d1a3f6ce
Nathan Bullock <nathanbullock@gmail.com>
2016-01-23 19:54:59 -0500

    Simple script to test submissions

---
+++ runner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for arg in $*; do
+  echo "Compiling $arg ..."
+
+  Q=`head -n 1 "$arg" | cut -d " " -f 2`
+  compgen -G "${Q}/*in" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "The first line of your code should be something like:"
+    echo "// 2015/s1" 
+  fi
+
+  g++ "$arg" -o tmp.exe
+  if [ $? -ne 0 ]; then
+    echo "Couldn't compile $arg"
+    exit 1
+  fi
+
+  for file in "${Q}"/*.in; do
+    echo "Running program with input data: $file"
+    ./tmp.exe < "$file" > tmp.out
+    diff tmp.out "${file%%in}out"
+    if [ $? -ne 0 ]; then
+      echo "Incorrect output see diff above."
+      exit 1
+    fi
+  done
+done
