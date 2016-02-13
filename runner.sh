95457fedfb52001c4ae0d2bf78ed2b184f9b661d
Nathan Bullock <nathanbullock@gmail.com>
2016-02-13 18:21:17 -0500

    Improve runner.py
    Compile code with C++ 11 and more warnings.
    Time how long it takes to run each test.

--- runner.sh
+++ runner.sh
@@ -3,6 +3,12 @@
 for arg in $*; do
   echo "Compiling $arg ..."
 
+  g++ -Wall -Werror -O1 -std=c++11 "$arg" -o tmp.exe
+  if [ $? -ne 0 ]; then
+    echo "Couldn't compile $arg"
+    exit 1
+  fi
+
   Q=`head -n 1 "$arg" | cut -d " " -f 2`
   compgen -G "${Q}/*in" > /dev/null
   if [ $? -ne 0 ]; then
@@ -11,19 +17,12 @@ for arg in $*; do
     exit 1
   fi
 
-  g++ "$arg" -o tmp.exe
-  if [ $? -ne 0 ]; then
-    echo "Couldn't compile $arg"
-    exit 1
-  fi
-
   for file in "${Q}"/*.in; do
     echo "Running program with input data: $file"
-    ./tmp.exe < "$file" > tmp.out
+    /usr/bin/time -f "Run time: %U" ./tmp.exe < "$file" > tmp.out
     diff tmp.out "${file%%in}out"
     if [ $? -ne 0 ]; then
       echo "Incorrect output see diff above."
-      exit 1
     fi
   done
 done
